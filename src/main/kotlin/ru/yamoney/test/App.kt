0412f8c893ae68de6623d23f1dd9efd1b24545a5
nizienko <nizienko@outlook.com>
2018-08-19 15:03:26 +0300

    Initial commit

---
+++ src/main/kotlin/ru/yamoney/test/App.kt
@@ -0,0 +1,14 @@
+
+package ru.yamoney.test
+
+const val USAGE_MESSAGE = """
+java -jar app.jar [COMMAND] [ARGUMENTS]
+            Commands:
+            payment %USER% %SUM% %SHOP_ID%
+            deposit %USER% %SUM%
+            balance %USER%
+            shop_info %SHOP_ID%
+"""
+
+
+
---
+++ src/test/kotlin/ru/yamoney/test/AppTest.kt
@@ -0,0 +1,25 @@
+package ru.yamoney.test
+
+
+class AppTest {
+
+/*    @Test
+    fun payment() {
+        main(arrayOf("payment", "Вася", "23", "4454"))
+    }
+
+    @Test
+    fun deposit() {
+        main(arrayOf("deposit", "Вася", "123", "4454"))
+    }
+
+    @Test
+    fun balance() {
+        main(arrayOf("balance", "Вася"))
+    }
+
+    @Test
+    fun shopInfo() {
+        main(arrayOf("shop_info", "4454"))
+    }*/
+}
